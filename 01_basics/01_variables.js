fbd0a8ac130c93e568c39a13f86a4b23675b3e41
jeet0108 <112840901+jeet0108@users.noreply.github.com>
2023-12-20 17:19:14 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 2445
+let accountEmail = "jeet@gmaol.com"
+var accountPassword ="34335"
+accountCity = "junagadh"
+
+accountEmail = "j@mail.com"
+accountPassword = "3254"
+accountCity = "morbi"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountId,accountCity])
