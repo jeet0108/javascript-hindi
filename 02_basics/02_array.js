00f755deab4e0551320ef9af18903cf4c027eb9e
jeet0108 <112840901+jeet0108@users.noreply.github.com>
2023-12-30 20:19:20 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,42 @@
+//for merging arrays
+
+const Array1 = [1,2,3,4,5];
+const Array2 = [5,6,7,8,9,10];
+const Array3 = [0,0,0];
+const Array4 = [1,3,9,19];
+const concatAll = Array1.concat(Array2, Array3,Array4)
+console.log(concatAll)
+
+//2.
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting -- empty
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
