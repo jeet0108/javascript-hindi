8e70a1c5933cb9fabbdfe68f9a097cfec27fe070
jeet0108 <112840901+jeet0108@users.noreply.github.com>
2023-12-30 19:48:31 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+const myArr = [1,2,3,4,5]
+// console.log(myArr);
+// console.log(myArr[2]);
+
+// const myArr1 = new Array(5,6,7,8)
+// console.log(myArr1);
+
+//array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// console.log(myArr);
+
+
+//slice and splice//
+
+console.log("a" ,myArr);
+
+const myn1 = myArr.slice(1,4)
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,4)
+console.log(myn2);
+console.log("c ", myArr);
+
+
+
